329fb038dbfd4cdbf05deecbd00be709ea25f673
Jubel Alberto Correa <JubelCorrea@MacBook-Pro-de-Jubel.local>
2016-07-15 12:53:46 -0500

    Index rendered dinamically

--- gallery.rb
+++ gallery.rb
@@ -1,11 +1,16 @@
 class Gallery
-	def initialize(url, pictures)
+	def initialize(url, name, pictures)
 		@url = url
 		@pictures = pictures
+		@name = name
 	end
 
 	def url
 		@url
+	end
+
+	def name
+		@name
 	end
 
 	def pictures
