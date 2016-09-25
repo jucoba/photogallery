bdb297124ed99e0e2b714935af60471f39695c29
Jubel Alberto Correa <JubelCorrea@MacBook-Pro-de-Jubel.local>
2016-09-24 21:49:12 -0500

    New post method

--- web.rb
+++ web.rb
@@ -6,12 +6,20 @@ require './picturesUrl.rb'
 
 get '/' do
 	@route = request.path_info
+	puts("request: "+@route)
 	erb:index
 end
 
 get '/*' do
+	puts "get"
 	@route = request.path_info
 	erb :antioquia
+end
+
+post '/test' do
+	puts "hola"
+	request.body.rewind
+	puts "body: "+request.body.read 
 end
 
 
